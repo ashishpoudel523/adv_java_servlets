ad33908314ec89ed331791722b0ed93071926115
Ashish Poudel <ashishpoudel523@gmail.com>
2022-01-20 15:47:37 +0545

    servlets

---
+++ src/main/java/FindBooksServlet.java
@@ -0,0 +1,46 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FindBooksServlet
+ */
+@SuppressWarnings("serial")
+@WebServlet("/booksfinder")
+public class FindBooksServlet extends HttpServlet {
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FindBooksServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+        out.println("Hello from FindBooks GET Method");
+        
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+        out.println("Hello from FindBooks POST Method");
+        
+	}
+
+}
